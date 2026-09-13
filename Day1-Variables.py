2d8b53005c50e1f83ec996a15b2f0319e1dd26ac
mansinikhade10 <nikhademansi5@gmail.com>
2026-09-14 00:54:16 +0530

    Day1-DataTypes&Variables

---
+++ Day1-Variables.py
@@ -0,0 +1,30 @@
+# What is variable?
+# Variables are the containers for storing data values.
+# Example
+name='mansi nikhade'#here name is the variable and mansi nikhade is the value stored in the name variable
+print(name)
+
+age = 23
+print(age)
+
+weight=50.5
+print(weight)
+
+# Python Data Types
+# There are multiple data types in python :
+# int :An integer is a whole number.
+print(type(age)) #output : <class 'int'>
+
+# float:A float represents a number containing a decimal component.
+print(type(weight))#output : <class 'float'>
+
+# str :A string is text or we can say sequence of characters
+print(type(name))#output : <class 'str'>
+
+# bool : Boolean has only two values: either true or false 
+adult=True
+print(type(adult))#output : <class 'bool'>
+
+# none :None means there is no value.
+semister=None
+print(type(semister))#output : <class 'NoneType'>
